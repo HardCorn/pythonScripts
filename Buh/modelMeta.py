586c58373861fafc3ae8c9c883cb9409e1edd95d
HardCorn <kuz-proper@list.ru>
2018-02-27 21:36:02 +0300

    funcs meta

--- Buh/modelMeta.py
+++ Buh/modelMeta.py
@@ -1,6 +1,7 @@
 import modelFile as mf
 import os
 import dates as dt
+import modelUtility as mu
 
 
 WORKERS_MODEL_NAME = 'Workers'
@@ -101,6 +102,10 @@ def get_meta_path(home_dir):
 def get_data_path(home_dir):
     return home_dir + 'Data\\'
 
+def get_worker_path(home_dir, worker_name):
+    data_path = get_data_path(home_dir)
+    return data_path + worker_name + '\\'
+
 
 def create_meta(home_dir=None):
     home_dir = home_dir or get_self_dir()
@@ -120,8 +125,50 @@ def create_meta(home_dir=None):
     return worker
 
 
-def add_model(model_header):
-    pass
+def add_worker(meta_worker : mf.ModelFileWorker, home_dir, worker_name):
+    path = get_worker_path(home_dir, worker_name)
+    meta_worker.insert_simple_data(WORKERS_MODEL_NAME, [worker_name, path],
+                                   [ATTR_WORKER_NAME, ATTR_WORKER_DIR])
+    return mf.ModelFileWorker(path)
+
+
+def add_model(meta_worker : mf.ModelFileWorker, worker_name, model_name, model_header):
+    lst_ = list()
+    parts = list()
+    condition = ATTR_MODEL_WORKER + ' = ' + worker_name + ' and ' + ATTR_MODEL_MODEL + ' = ' + model_name
+    fltr = mu.Filter()
+    fltr.set_clause(condition)
+    id = meta_worker.read_model_data(IDS_MODEL_NAME, filter_=fltr, selected=ATTR_ID_GEN_MAX)
+    if len(id) == 0:
+        id = 0
+    else:
+        id = id[0][0]
+    for each in model_header[mf.PARTITION_ATTRIBUTE_KEY]:
+        parts.append(model_header[mf.PARTITION_ATTRIBUTE_KEY][each][mf.PARTITION_FIELD_NUM])
+    for each in model_header[mf.ATTRIBUTE_KEY]:
+        id += 1
+        attr = model_header[mf.ATTRIBUTE_KEY][each]
+        inn_lst = list()
+        inn_lst.append(id)
+        inn_lst += [worker_name, model_name, attr[mf.ATTRIBUTE_NAME_KEY], attr[mf.ATTRIBYTE_TYPE_KEY]]
+        if each in parts:
+            inn_lst.append(1)
+        else:
+            inn_lst.append(0)
+        inn_lst += [attr[mf.OPTION_HIDE_KEY], attr[mf.OPTION_DEFAULT_KEY]]
+        lst_.append(inn_lst)
+    attr_list = [ATTR_ID, ATTR_MODEL_WORKER, ATTR_MODEL_MODEL, ATTR_MODEL_ATTR_NAME, ATTR_MODEL_ATTR_TYPE,
+                 ATTR_MODEL_PARTITION, ATTR_MODEL_HIDE, ATTR_MODEL_DEFAULT]
+    meta_worker.write_model_data(MODELS_MODEL_NAME, lst_, attr_list, brutal=False)
+    attr_list = [ATTR_MODEL_WORKER, ATTR_MODEL_MODEL, ATTR_ID_GEN_MAX]
+    fltr.set_clause('not (' + condition + ')')
+    id_list = meta_worker.read_model_data(IDS_MODEL_NAME, filter_=fltr, selected=attr_list)
+    id_list.append([worker_name, model_name, id])
+    meta_worker.write_model_data(IDS_MODEL_NAME, id_list, attr_list, brutal=True)
+
+
+
+
 
 
 if __name__ == '__main__':
